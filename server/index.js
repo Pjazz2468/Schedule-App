a2955afdfbd3bde5328da87165d929c60f0daee4
Pjazz2468 <ogungbenrop@gmail.com>
2026-06-10 00:54:41 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use('/api/shares', sharesRoutes);
 
 if (isProd) {
   app.use(express.static(path.join(__dirname, '../dist')));
-  app.get('*', (req, res) => {
+  app.get('(.*)', (req, res) => {
     res.sendFile(path.join(__dirname, '../dist', 'index.html'));
   });
 }
